d52e6bfa49c64e311c2499a530ae0c16ce888065
luckyLacchin <gabrielelacchin92@gmail.com>
2023-03-29 18:28:36 +0200

    to continue zaino algorithm

--- 19_zaino/zaino.cc
+++ 19_zaino/zaino.cc
@@ -1,11 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 int main () {
 
-    
+    ifstream in("input.txt");
+    ofstream out("output.txt");
 
 
     return 0;
+}
+
+int knapsack (int w[], int p[], int n, int C) {
+    int **DP = new int *[n];
+    for (int i = 0; i < n; i++)
+        DP[i] = new int [C];
+    
+    for (int c = 0; c < C; c++) {
+        DP[0][c] = 0;
+    }
+    for (int i = 0; i < n; i++) {
+        DP[i][0] = 0;
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int c = 1; c < C; c++) {
+
+        }
+    }
 }
